fb88a1e1a949422d7a8a50c4250956d863092b81
wardaddy <josel.marquez.almeyda@gmail.com>
2021-10-13 00:38:30 -0500

    server tz

--- src/main/java/pe/sag/routing/RoutingApplication.java
+++ src/main/java/pe/sag/routing/RoutingApplication.java
@@ -5,8 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.Scheduled;
 import pe.sag.routing.core.scheduling.OrderTaskScheduler;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RoutingApplication {
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT-5"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(RoutingApplication.class, args);
