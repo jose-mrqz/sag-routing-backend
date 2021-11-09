f33f9916e7de3a66927204adaeeda15e42a094a1
egarma27 <egarma99@gmail.com>
2021-11-08 23:13:05 -0500

    Usuario y pedidos listos

--- src/main/java/pe/sag/routing/api/request/NewOrderRequest.java
+++ src/main/java/pe/sag/routing/api/request/NewOrderRequest.java
@@ -4,12 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class NewOrderRequest {
+    private int code;
+    @NotBlank
     private int x;
+    @NotBlank
     private int y;
+    @NotBlank
     private double demandGLP;
+    @NotBlank
     private int slack;
 }
