3ab3843cb9e5336547fba92537fea8a3e0bb4ae0
egarma27 <egarma99@gmail.com>
2021-11-13 20:22:46 -0500

    Listar pedidos con filtros listos

---
+++ src/main/java/pe/sag/routing/api/request/ListOrderRequest.java
@@ -0,0 +1,15 @@
+package pe.sag.routing.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ListOrderRequest {
+    private String filter;
+    private LocalDateTime date;
+}
