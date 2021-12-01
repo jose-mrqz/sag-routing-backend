ff5d6759e35ad7b744b88284893b9f14806a50e5
wardaddy <josel.marquez.almeyda@gmail.com>
2021-12-01 01:14:50 -0500

    carajo

--- src/main/java/pe/sag/routing/api/controller/OrderController.java
+++ src/main/java/pe/sag/routing/api/controller/OrderController.java
@@ -353,12 +353,14 @@ public class OrderController {
         JRBeanCollectionDataSource beanCollectionDataSource = new JRBeanCollectionDataSource(orders);
         //JRBeanArrayDataSource beanCollectionDataSource = new JRBeanArrayDataSource(orders.toArray());
 
-        JRDataSource compileReportEmpty = new JREmptyDataSource(1);
-        //JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream(System.getProperty("user.dir") + "/reportes/ReportePedidos.jrxml"));
+        JRDataSource compileReportEmpty = new JREmptyDataSource();
+//        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream(System.getProperty("user.dir") + "/reportes/ReportePedidos.jrxml"));
 //        File file  = ResourceUtils.getFile("/home/arch/reportes/ReportePedidos.jrxml");
         InputStream resource = getClass().getResourceAsStream("/ReportePedidos.jrxml");
 //        URL jarUrl = new URL("jar:file:/home/arch/sag-routing-backend/target/routing-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/reportes/ReportePedidos.jrxml");
 //        JasperReport compileReport = JasperCompileManager.compileReport(new FileInputStream(file.getAbsolutePath()));
+//        JasperReport compileReport = JasperCompileManager.compileReport(resource);
+//        JasperReport compileReport = JasperCompileManager.compileReport(getClass().getResourceAsStream("/reportes/ReportePedidos.jrmlx"));
         JasperReport compileReport = JasperCompileManager.compileReport(resource);
         JRSaver.saveObject(compileReport, "ReportePedidos.jasper");
 
