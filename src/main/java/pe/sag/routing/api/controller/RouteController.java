7b24ed9cfccbf29cdcd9d7596f5451c0b9f975dd
jose <josel.marquez.almeyda@gmail.com>
2021-12-02 23:20:26 -0500

    push

--- src/main/java/pe/sag/routing/api/controller/RouteController.java
+++ src/main/java/pe/sag/routing/api/controller/RouteController.java
@@ -256,16 +256,16 @@ public class RouteController {
                 LocalDateTime start = startDateReal;
                 LocalDateTime end;
                 LocalDateTime aux;
-                List<Order> pendingOrders;
-                LocalDateTime lastOrder = RouteController.simulationHelper.getLastDate();
-                while (true) {
-                    end = start.plusMinutes(60);
-                    pendingOrders = orderService.getByDateSimulation(start, end);
-                    aux = start;
-                    start = end;
-                    if (pendingOrders.size() != 0 || (end.isAfter(lastOrder) || end.isEqual(lastOrder))) break;
-                }
-//                List<Order> pendingOrders = orderService.getBatchedByStatusMonitoring(OrderStatus.PENDIENTE, false);
+//                List<Order> pendingOrders;
+//                LocalDateTime lastOrder = RouteController.simulationHelper.getLastDate();
+//                while (true) {
+//                    end = start.plusMinutes(60);
+//                    pendingOrders = orderService.getByDateSimulation(start, end);
+//                    aux = start;
+//                    start = end;
+//                    if (pendingOrders.size() != 0 || (end.isAfter(lastOrder) || end.isEqual(lastOrder))) break;
+//                }
+                List<Order> pendingOrders = orderService.getBatchedByStatusMonitoring(OrderStatus.PENDIENTE, false);
                 if (pendingOrders.size() == 0) {
                     RouteController.simulationData.setNScheduled(simulationData.getNOrders());
                     RouteController.simulationData.setMessage("Simulacion terminada con exito.");
@@ -283,8 +283,8 @@ public class RouteController {
                         LocalDateTime endTime = lastRoute.getFinishDate();
                         truck.setLastRouteEndTime(endTime);
                     }
-//                    else truck.setLastRouteEndTime(startDateReal);
-                    else truck.setLastRouteEndTime(aux);
+                    else truck.setLastRouteEndTime(startDateReal);
+//                    else truck.setLastRouteEndTime(aux);
                 }
 
                 if (pendingOrders.size() != 0 && availableTrucks.size() != 0) {
@@ -359,17 +359,17 @@ public class RouteController {
 
         LocalDateTime start = RouteController.simulationHelper.getStartDate();
         LocalDateTime end;
-        List<Order> pendingOrders;
-        LocalDateTime lastOrder = RouteController.simulationHelper.getLastDate();
-        while (true) {
-            end = start.plusMinutes(60);
-            pendingOrders = orderService.getByDateSimulation(start, end);
-            start = end;
-            if (pendingOrders.size() != 0 || (end.isAfter(lastOrder) || end.isEqual(lastOrder))) break;
-        }
+//        List<Order> pendingOrders;
+//        LocalDateTime lastOrder = RouteController.simulationHelper.getLastDate();
+//        while (true) {
+//            end = start.plusMinutes(60);
+//            pendingOrders = orderService.getByDateSimulation(start, end);
+//            start = end;
+//            if (pendingOrders.size() != 0 || (end.isAfter(lastOrder) || end.isEqual(lastOrder))) break;
+//        }
 
         List<Truck> availableTrucks = truckService.findByMonitoringAndStatus(false, TruckStatus.DISPONIBLE);
-//        List<Order> pendingOrders = orderService.getBatchedByStatusMonitoring(OrderStatus.PENDIENTE, false);
+        List<Order> pendingOrders = orderService.getBatchedByStatusMonitoring(OrderStatus.PENDIENTE, false);
 
         for (Truck truck : availableTrucks) {
             Route lastRoute = routeService.getLastRouteByTruckMonitoring(truck, false);
